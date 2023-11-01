8a8da195b5c973e7cb72f77c81706bcb54eaac30
Werefox <jack@wrightus.com>
2023-11-01 19:41:46 -0400

    Stubbed some basic inventory scripts

---
+++ Runtime/Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxthorne.FoxInventory
+{
+	public class Inventory : MonoBehaviour
+	{
+
+	}
+}
---
+++ Runtime/ScriptableObjects/SO_ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxthorne.FoxInventory
+{
+    public class SO_ItemData : ScriptableObject
+    {
+        public string itemName;
+        public int maxStackSize = 100;
+    }
+}
